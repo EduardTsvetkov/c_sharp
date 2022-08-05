387618f495decdfaa6d3c8c5ac984a612714be37
EduardTsvetkov <edvardas@inbox.ru>
2022-08-05 20:57:35 +0300

    Example005_CnditionIfElse project

---
+++ Example005_CnditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+if(userName.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(userName);
+    Console.WriteLine("! А где Маша?");
+}
+
