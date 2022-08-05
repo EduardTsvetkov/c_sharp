37a54719a7b09c40a658ec3902d44d29c8386b59
EduardTsvetkov <edvardas@inbox.ru>
2022-08-05 20:20:59 +0300

    Example004_Div project

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,8 @@
+﻿double numberA = 12;
+double numberB = 5;
+double result = numberA / numberB;
+Console.Write(numberA);
+Console.Write(" / ");
+Console.Write(numberB);
+Console.Write(" = ");
+Console.Write(result);
