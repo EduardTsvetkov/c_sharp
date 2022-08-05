9e6bc9056d7160e277bf0e1a3a6d7988f495bd01
EduardTsvetkov <edvardas@inbox.ru>
2022-08-05 20:12:41 +0300

    Example003_Sum project

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,8 @@
+﻿int numberA = 31;
+int numberB = 5;
+int result = numberA + numberB;
+Console.Write(numberA);
+Console.Write(" + ");
+Console.Write(numberB);
+Console.Write(" = ");
+Console.Write(result);
