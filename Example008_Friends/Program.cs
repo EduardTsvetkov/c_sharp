224bc6ad4f6f30f785875bb503f269db8bd0e2f3
EduardTsvetkov <edvardas@inbox.ru>
2022-08-06 14:06:06 +0300

    Example008_Friends project

---
+++ Example008_Friends/Program.cs
@@ -0,0 +1,24 @@
+﻿int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int time = 0;
+int count = 0;
+int friendNumber = 2;
+
+while (distance > 10)
+{
+    if (friendNumber == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friendNumber = 2;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friendNumber = 1;
+    }
+    count++;
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+}
+Console.Write("Собака пробежит " + count + " раз.");
