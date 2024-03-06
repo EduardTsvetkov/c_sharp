c0d5e21ac0be5b6d10a13ee70bf80b86f631bd00
EduardTsvetkov <109814102+EduardTsvetkov@users.noreply.github.com>
2024-03-06 11:30:34 +0300

    Delete Example003_Sum directory

--- Example003_Sum/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿int numberA = new Random().Next(1, 10);   // 1, 2, 3, ..., 9
-int numberB = new Random().Next(1, 10);
-int result = numberA + numberB;
-Console.Write(numberA);
-Console.Write(" + ");
-Console.Write(numberB);
-Console.Write(" = ");
-Console.Write(result);
